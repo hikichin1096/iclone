ac1becb66aa6d5f6584978995252a6af1adf2d0c
Wataru Hikichi <w.hikichi.kg@gmail.com>
2019-12-01 16:11:24 +0900

    詳細画面表示

--- app/controllers/insclones_controller.rb
+++ app/controllers/insclones_controller.rb
@@ -2,7 +2,7 @@ class InsclonesController < ApplicationController
 
   #インデックスアクション
   def index
-    
+
     #テーブルから全ての投稿を取得
     @insclones = Insclone.all
   end
@@ -12,12 +12,18 @@ class InsclonesController < ApplicationController
     @insclone = Insclone.new
   end
 
-  #作成メソッド
+  #作成
   def create
     @insclone = Insclone.create(insclone_params)
 
     #新規画面に移動
     redirect_to new_insclone_path
+  end
+
+  #表示
+  def show
+    #指定idのパラメータ取得
+    @insclone = Insclone.find(params[:id])
   end
 
   private
